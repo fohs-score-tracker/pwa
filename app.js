c99630a73c6257bffe51901c0005be172dd6ac3a
Wesley Henson <whenson9086@student.fortosage.net>
2021-05-20 22:47:41 -0500

    Add simple login page
    TODO: Add picture / welcome text

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var app = Vue.createApp({
     data() {
         return {
-            players: [],
+            apiBase: "https://fohs-score-tracker.herokuapp.com",
+            authHeader: "",
+            email: "",
+            loginError: "",
             newPlayerName: "",
-            apiEndpoint: "https://fohs-score-tracker.herokuapp.com",
+            password: "",
+            players: [],
         }
     },
-    created() {
-        this.loadPlayers();
+    mounted() {
+        $('#login').modal({
+            backdrop: "static",
+            keyboard: false
+        });
     },
     methods: {
-        apiCall(path, args) {
-            return fetch(this.apiEndpoint + path, args);
+        apiCall(path, args = {}) {
+            if (!args.headers)
+                args.headers = {};
+            args.headers["Authorization"] = this.authHeader;
+            return fetch(this.apiBase + path, args);
         },
         addPlayer() {
             if (this.newPlayerName != "") {
@@ -31,6 +41,23 @@ var app = Vue.createApp({
         },
         loadPlayers() {
             this.apiCall("/players").then(f => f.json()).then(j => this.players = j);
+        },
+        login() {
+            if (!$("#input-email")[0].checkValidity()) {
+                this.loginError = "Please enter a valid email address";
+                return;
+            } else if (!$("#input-password")[0].checkValidity()) {
+                this.loginError = "Please enter a password.";
+                return;
+            }
+            // TODO: save login info somewhere
+            this.authHeader = `Basic ${btoa(`${this.email}:${this.password}`)}`;
+            this.apiCall("/users/me").then(r => {
+                if (r.ok)
+                    $("#login").modal("hide");
+                else
+                    r.json().then(j => this.loginError = j.detail + ".");
+            }).catch(e => this.loginError = "Can't connect to server.");
         }
     },
     provide() {
