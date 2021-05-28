96e5a734648a4d86ef6c015e8e2daa791b80b992
Wesley Henson <whenson9086@student.fortosage.net>
2021-05-28 12:10:08 -0500

    use 4 spaces instead of 2

--- app.js
+++ app.js
@@ -11,29 +11,28 @@ var app = Vue.createApp({
             password: "",
             passwordHint: "",
             players: [],
-            waitingForLogin: false
-        }
+            waitingForLogin: false,
+        };
     },
     mounted() {
         this.loginModal = new bootstrap.Modal(this.$refs.login, {
             keyboard: false,
-            backdrop: 'static'
+            backdrop: "static",
         });
         this.loginModal.show();
 
-        var canvas = document.getElementById('main-canvas'),
-            context = canvas.getContext('2d');
+        var canvas = document.getElementById("main-canvas"),
+            context = canvas.getContext("2d");
 
         base_image = new Image();
-        base_image.src = './court-placeholder.png';
+        base_image.src = "./court-placeholder.png";
         base_image.onload = function () {
             context.drawImage(base_image, 0, 0);
-        }
+        };
     },
     methods: {
         apiCall(path, args = {}) {
-            if (!args.headers)
-                args.headers = {};
+            if (!args.headers) args.headers = {};
             args.headers["Authorization"] = this.authHeader;
             return fetch(this.apiBase + path, args);
         },
@@ -45,16 +44,20 @@ var app = Vue.createApp({
                 this.newPlayerName = "";
                 this.apiCall("/players/new", {
                     method: "POST",
-                    body: JSON.stringify(payload)
-                })
-                    .then(p => p.ok
-                        ? p.json().then(j => this.players.push(j))
-                        : p.json().then(j => console.warn("%o: %o", payload, j))
-                    );
+                    body: JSON.stringify(payload),
+                }).then((p) =>
+                    p.ok
+                        ? p.json().then((j) => this.players.push(j))
+                        : p
+                              .json()
+                              .then((j) => console.warn("%o: %o", payload, j))
+                );
             }
         },
         loadPlayers() {
-            this.apiCall("/players").then(f => f.json()).then(j => this.players = j);
+            this.apiCall("/players")
+                .then((f) => f.json())
+                .then((j) => (this.players = j));
         },
         login() {
             this.emailHint = "";
@@ -70,30 +73,32 @@ var app = Vue.createApp({
             // TODO: save login info somewhere
             this.authHeader = `Basic ${btoa(`${this.email}:${this.password}`)}`;
             this.waitingForLogin = true;
-            this.apiCall("/users/me").then(r => {
-                if (r.ok) {
-                    this.loginModal.hide();
-                    this.loadPlayers();
-                } else {
-                    r.json().then(j => {
-                        if (j.detail.includes("password"))
-                            this.passwordHint = j.detail + "."
-                        else
-                            this.emailHint = j.detail + "."
-                    });
-                }
-            }).catch(e => {
-                this.loginWarning = true;
-            }).finally(() => {
-                this.waitingForLogin = false;
-            });
-        }
+            this.apiCall("/users/me")
+                .then((r) => {
+                    if (r.ok) {
+                        this.loginModal.hide();
+                        this.loadPlayers();
+                    } else {
+                        r.json().then((j) => {
+                            if (j.detail.includes("password"))
+                                this.passwordHint = j.detail + ".";
+                            else this.emailHint = j.detail + ".";
+                        });
+                    }
+                })
+                .catch((e) => {
+                    this.loginWarning = true;
+                })
+                .finally(() => {
+                    this.waitingForLogin = false;
+                });
+        },
     },
     provide() {
         return {
-            apiCall: this.apiCall
-        }
-    }
+            apiCall: this.apiCall,
+        };
+    },
 });
 
 app.component("player", {
@@ -105,12 +110,16 @@ app.component("player", {
     },
     methods: {
         removePlayer() {
-            this.apiCall(`/players/${this.id}`, { method: "DELETE" }).then(r => {
-                if (r.ok)
-                    this.$root.players = this.$root.players.filter(p => p.id != this.id);
-            });
-        }
-    }
+            this.apiCall(`/players/${this.id}`, { method: "DELETE" }).then(
+                (r) => {
+                    if (r.ok)
+                        this.$root.players = this.$root.players.filter(
+                            (p) => p.id != this.id
+                        );
+                }
+            );
+        },
+    },
 });
 
 var vm;
